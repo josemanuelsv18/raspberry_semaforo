85531e2853939742475377c2fd628563c02d00fd
Josemanuel Sifontes <josemanuelsv18@gmail.com>
2024-09-13 06:53:48 -0500

    Programa finalizado falta verificacion con raspberry en fisico

--- Main.py
+++ Main.py
@@ -6,7 +6,22 @@ class Main:
         pass
     
     def main(self):
-        pass
+        #Cambiar el None por los valores de los pins que se conecten a los leds en la placa
+        #variables para el primer semaforo
+        pin_rojo1 = None
+        pin_amarillo1 = None
+        pin_verde1= None
+        semaforo1 = [True, pin_rojo1, pin_amarillo1, pin_verde1]
+        #variables para el segundo semaforo
+        pin_rojo2 = None
+        pin_amarillo2 = None
+        pin_verde2 = None
+        semaforo2 = [False, pin_rojo2,pin_amarillo2,pin_verde2]
+        #Crear los obj semaforos y llamar los metodos
+        obj_semaforo1 = Semaforo.semaforo(semaforo1[0],semaforo1[1],semaforo1[2],semaforo1[3])
+        obj_semaforo2 = Semaforo.semaforo(semaforo2[0],semaforo2[1],semaforo2[2],semaforo2[3])
+        obj_semaforo1.encender_semaforo()
+        obj_semaforo2.encender_semaforo()
 
 #llamada al main
 if __name__ == '__main__':
--- Semaforo.py
+++ Semaforo.py
@@ -3,11 +3,12 @@ import time
 
 class semaforo:
     #metodo constructor
-    def __init__(self, r, a, v): #recibe el numero del pin al que esta conectado cada led
+    def __init__(self, r, a, v, inicial): #recibe el numero del pin al que esta conectado cada led
         #atributos
         self.rojo = gpiozero.LED(r)
         self.amarillo = gpiozero.LED(a)
         self.verde = gpiozero.LED(v)
+        self.inicial = inicial
 
     #getters
     def get_rojo(self):
@@ -16,31 +17,39 @@ class semaforo:
         return self.amarillo
     def get_verde(self):
         return self.verde
+    def get_inicial(self):
+        return self.inicial
 
-    def encender_semaforo(self, verde): #verde parametro booleano que determina si la posicion inicial del semaforo es o no verde
-        if verde:
+    def encender_semaforo(self): #verde parametro booleano que determina si la posicion inicial del semaforo es o no verde
+        if self.inicial:
             while True:
                 self.verde.on
+                print('Semaforo 1 VERDE')
                 time.sleep(5)
                 self.verde.off
 
                 self.amarillo.on
+                print('Semaforo 1 AMARILLO')
                 time.sleep(2)
                 self.amarillo.off
 
                 self.rojo.on
+                print('Semaforo 1 ROJO')
                 time.sleep(7)
                 self.rojo.off
         else:
             while True:
                 self.rojo.on
+                print('Semaforo 2 ROJO')
                 time.sleep(7)
                 self.rojo.off
 
                 self.verde.on
+                print('Semaforo 2 VERDE')
                 time.sleep(5)
                 self.verde.off
 
                 self.amarillo.on
+                print('Semaforo 2 AMARILLO')
                 time.sleep(2)
                 self.amarillo.off
