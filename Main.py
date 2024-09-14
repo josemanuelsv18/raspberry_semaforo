b5b9ab4b6a090f0f6a5eca3060d53cfe7d294b8b
Josemanuel Sifontes <josemanuelsv18@gmail.com>
2024-09-14 16:23:26 -0500

    Se cambia al uso de la libreria machine

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 import Semaforo
-import Boot
+#import Boot
 
 class Main:
     #inicializacion de la clase
@@ -9,20 +9,20 @@ class Main:
     def main(self):
         #Cambiar el input por el numero del pin que asociado a los leds
         #variables para el primer semaforo
-        pin_rojo1 = input('Ingrese el numero de pin para el led rojo del semaforo 1')
-        pin_amarillo1 = input('Ingrese el numero de pin para el led amarillo del semaforo 1')
-        pin_verde1= input('Ingrese el numero de pin para el led verde del semaforo 1')
+        pin_rojo1 = 0
+        pin_amarillo1 = 1
+        pin_verde1= 2
         semaforo1 = [True, pin_rojo1, pin_amarillo1, pin_verde1]
         #variables para el segundo semaforo
-        pin_rojo2 = input('Ingrese el numero de pin para el led rojo del semaforo 2')
-        pin_amarillo2 = input('Ingrese el numero de pin para el led amarillo del semaforo 2')
-        pin_verde2 = input('Ingrese el numero de pin para el led amarillo del semaforo 2')
+        pin_rojo2 = 5
+        pin_amarillo2 = 4
+        pin_verde2 = 3
         semaforo2 = [False, pin_rojo2,pin_amarillo2,pin_verde2]
         #Crear y establecer conexion Wi-Fi
-        ssid = input('Ingrese el nombre de la red Wi-Fi')
-        password = input('Ingrese la contraseña de la red')
-        obj_conexion = Boot.Boot(ssid, password)
-        obj_conexion.conectar()
+        #ssid = input('Ingrese el nombre de la red Wi-Fi')
+        #password = input('Ingrese la contraseña de la red')
+        #obj_conexion = Boot.Boot(ssid, password)
+        #obj_conexion.conectar()
         #Crear los obj semaforos y llamar los metodos  
         obj_semaforo1 = Semaforo.semaforo(semaforo1[0],semaforo1[1],semaforo1[2],semaforo1[3])
         obj_semaforo2 = Semaforo.semaforo(semaforo2[0],semaforo2[1],semaforo2[2],semaforo2[3])
--- Semaforo.py
+++ Semaforo.py
@@ -1,13 +1,13 @@
-import gpiozero
+from machine import Pin
 import time
 
 class semaforo:
     #metodo constructor
     def __init__(self,inicial,r, a, v): #recibe el numero del pin al que esta conectado cada led
         #atributos
-        self.rojo = gpiozero.LED(r)
-        self.amarillo = gpiozero.LED(a)
-        self.verde = gpiozero.LED(v)
+        self.rojo = Pin(r, Pin.OUT)
+        self.amarillo = Pin(a, Pin.OUT)
+        self.verde = Pin(v, Pin.OUT)
         self.inicial = inicial
 
     #getters
@@ -23,33 +23,37 @@ class semaforo:
     def encender_semaforo(self): #verde parametro booleano que determina si la posicion inicial del semaforo es o no verde
         if self.inicial:
             while True:
-                self.verde.on
+                self.verde.on()
+                self.amarillo.off()
+                self.get_rojo.off()
                 print('Semaforo 1 VERDE')
                 time.sleep(5)
-                self.verde.off
+                self.verde.off()
 
-                self.amarillo.on
+                self.amarillo.on()
                 print('Semaforo 1 AMARILLO')
                 time.sleep(2)
-                self.amarillo.off
+                self.amarillo.off()
 
-                self.rojo.on
+                self.rojo.on()
                 print('Semaforo 1 ROJO')
                 time.sleep(7)
-                self.rojo.off
+                self.rojo.off()
         else:
             while True:
-                self.rojo.on
+                self.rojo.on()
+                self.amarillo.off()
+                self.verde.off()
                 print('Semaforo 2 ROJO')
                 time.sleep(7)
-                self.rojo.off
+                self.rojo.off()
 
-                self.verde.on
+                self.verde.on()
                 print('Semaforo 2 VERDE')
                 time.sleep(5)
-                self.verde.off
+                self.verde.off()
 
-                self.amarillo.on
+                self.amarillo.on()
                 print('Semaforo 2 AMARILLO')
                 time.sleep(2)
-                self.amarillo.off
+                self.amarillo.off()
